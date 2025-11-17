32653eca718e3794c4d4be4be25867f0c5482eac
Rahmat Fajar Saputra <fajar.saputra2907@gmail.com>
2025-11-17 23:14:13 +0700

    Styling Optimization

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -17,11 +17,8 @@ export const Navbar = () => {
   ];
 
   return (
-    <motion.nav 
+    <nav
       className="fixed top-0 left-0 right-0 z-50 bg-purple-200/80 backdrop-blur-sm shadow-md antialiased font-sans"
-      initial={{ y: -100 }}
-      animate={{ y: 0 }}
-      transition={{ duration: 0.5 }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
@@ -77,6 +74,6 @@ export const Navbar = () => {
           </button>
         </div>
       </div>
-    </motion.nav>
+    </nav>
   );
 };
